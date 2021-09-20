47b6b449abe7a1ac2f587d16b7f4ba1004fb2a98
Joyal Jose <jjneutron@outlook.com>
2021-09-20 12:16:44 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,7 +4,6 @@ export WITH_GMS=true
 
 cd /mnt/disk1/$rom
 
-./rom-build.sh -t $buildtype $device > build.log
-
+./rom-build.sh -t $buildtype $device -j 16
 cd out/target/product/$device
 rclone copy pa-ruby-1-vayu-*-dev.zip drive:ROMs/vayu/PA/11
